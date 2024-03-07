21d137e98ae237d5f7dcd5457f9b57523932191e
QuillG <quentin.guillemand@gmail.com>
2024-03-07 15:00:55 +0100

    add import

--- app/src/main/java/com/example/knifeskincs/data/dao/KnifeDao.java
+++ app/src/main/java/com/example/knifeskincs/data/dao/KnifeDao.java
@@ -1,6 +1,7 @@
 package com.example.knifeskincs.data.dao;
 
 import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -10,6 +11,7 @@ import com.example.knifeskincs.data.models.Knife;
 
 import java.util.List;
 
+@Dao
 public interface KnifeDao {
     @Insert
     void insertKnife(Knife... knifes);
